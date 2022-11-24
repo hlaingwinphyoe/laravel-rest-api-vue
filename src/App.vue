0bbe3cf1f963104d5b57cebd60461d1280df589a
hlaingwinphyoe <hlaingwinphyoedev@gmail.com>
2022-11-24 10:20:09 +0630

    v1

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="mx-auto max-w-7xl">
+      <nav class="p-2">
+        <RouterLink to="/" class="px-4 py-2 mr-4 text-white bg-indigo-600 rounded hover:bg-indigo-800">Home</RouterLink>
+        <RouterLink to="/skills" class="px-4 py-2 mr-4 text-white bg-indigo-600 rounded hover:bg-indigo-800">Skills</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <main class="min-h-screen mx-auto max-w-7xl">
+    <RouterView />
+  </main>
+</template>
---
+++ src/views/skills/SkillCreate.vue
@@ -0,0 +1,42 @@
+
+<script setup>
+    import { reactive } from "vue";
+    import useSkills from "../../composables/skills";
+
+    const { storeSkill,errors } = useSkills();
+
+    const form = reactive({
+        name: "",
+        slug: "",
+    });
+</script>
+
+<template>
+    <div class="mt-12">  
+        <form class="max-w-md p-4 mx-auto bg-white rounded-md shadow-md" @submit.prevent="storeSkill(form)">
+            <div class="space-y-6">
+                <div class="mb-6">
+                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                    <input type="text" v-model="form.name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <div v-if="errors.name">
+                        <span class="text-sm text-red-400">{{ errors.name[0] }}</span>
+                    </div>
+                </div>
+                <div class="mb-6">
+                    <label for="slug" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Slug</label>
+                    <input type="text" v-model="form.slug" id="slug" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <div v-if="errors.slug">
+                        <span class="text-sm text-red-400">{{ errors.slug[0] }}</span>
+                    </div>
+                </div>
+                <div class="mt-4">
+                    <button type="submit" class="px-4 py-2 text-white bg-indigo-600 rounded hover:bg-indigo-700">Store</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
